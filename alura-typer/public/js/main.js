2b11fcdd5fb55d627e92bad4cd61382ea99edd00
Yuri <yurifdev@gmail.com>
2022-12-29 00:00:36 -0300

    Making the counting of caracters and words

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -5,5 +5,20 @@ var tamanhoFrase = $("#tamanho-frase")
 
 // tamanhoFrase.text() Diz o que está escrito
 tamanhoFrase.text(numPalavras) //Muda o que está escrito
-console.log(tamanhoFrase);
-console.log(frase);
+
+var campo = $(".campo-digitacao");
+// campo.on('click', () => {
+campo.on('input', () => { //Evento de dado sendo adicionado em qualquer input
+    var conteudo = campo.val() //.val dá acesso ao que está dentro de uma tag de input
+    var qtdPalavras = conteudo.split(/\S+/g).length;
+    if (qtdPalavras === 1) {
+        qtdPalavras = 0
+    }
+    $("#contador-palavras").text(qtdPalavras)
+    
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres)
+
+    console.log(qtdPalavras);
+    // console.log('caracteres', conteudo.length);
+})
