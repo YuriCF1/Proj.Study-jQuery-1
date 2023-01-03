87a09f08217489497e28aa1fb44c7cabb170318e
Yuri <yurifdev@gmail.com>
2023-01-03 19:06:27 -0300

    Creating the spinner on loading

--- alura-typer/public/js/shuffleSentences.js
+++ alura-typer/public/js/shuffleSentences.js
@@ -11,12 +11,15 @@ function fraseAleatoria() {
   //     "dataType"
   //   );
 
+  $("#spinner").toggle()
   $.get("http://localhost:3000/frases", trocaFraseAleatoria).fail(() => {
     $("#error").toggle();
     setTimeout(() => {
       $("#error").toggle();
     }, 2000);
-  });
+  }).always(() => {
+    $("#spinner").toggle()
+  })
 }
 
 function trocaFraseAleatoria(data) {
