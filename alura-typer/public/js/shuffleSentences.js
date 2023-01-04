ebea67ac5424187b1dfe8052f472e746d9429f70
Yuri <yurifdev@gmail.com>
2023-01-04 10:19:43 -0300

    Creating button and function to get a sentence form a user ID

--- alura-typer/public/js/shuffleSentences.js
+++ alura-typer/public/js/shuffleSentences.js
@@ -2,6 +2,7 @@
 //FAZER TODAS AS FRASES SEGUIDAS
 
 $("#btn-frase").click(fraseAleatoria);
+$("#btn-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
   //   $.get("url", data,
@@ -11,15 +12,17 @@ function fraseAleatoria() {
   //     "dataType"
   //   );
 
-  $("#spinner").toggle()
-  $.get("http://localhost:3000/frases", trocaFraseAleatoria).fail(() => {
-    $("#error").toggle();
-    setTimeout(() => {
+  $("#spinner").toggle();
+  $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(() => {
       $("#error").toggle();
-    }, 2000);
-  }).always(() => {
-    $("#spinner").toggle()
-  })
+      setTimeout(() => {
+        $("#error").toggle();
+      }, 2000);
+    })
+    .always(() => {
+      $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
@@ -31,4 +34,27 @@ function trocaFraseAleatoria(data) {
   atualizaTempoInicial(data[numeroAleatorio].tempo);
   // inicializaCronometro()
   // reiniciaJogo()
+}
+
+function buscaFrase() {
+  $("#spinner").toggle();
+  var fraseId = $("#frase-id").val();
+  var dados = { id: fraseId }; //{id: '1'}
+  console.log(dados);
+  $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(() => {
+      $("#error").toggle();
+      setTimeout(() => {
+        $("#error").toggle();
+      }, 2000);
+    })
+    .always(() => {
+      $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+  $(".frase").text(data.texto);
+  atualizaTamanhoFrase()
+  atualizaTempoInicial(data.tempo)
 }
